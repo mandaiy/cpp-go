89f16fb8380d0edd169eeaf717ee93e01cc4fec8
Yusaku Mandai <mandai@graco.c.u-tokyo.ac.jp>
2018-09-10 20:49:43 +0900

    v0.6.1

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='cppgo',
-    version='0.6.0',
+    version='0.6.1',
     author='Yusaku Mandai',
     author_email='mandai@graco.c.u-tokyo.ac.jp',
     description='Go library implemented in C++',
